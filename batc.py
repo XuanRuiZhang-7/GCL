ddcc0da29831cb2f2c3a15fee461a12f526aea69
XuanRuiZhang-7 <74554891+XuanRuiZhang-7@users.noreply.github.com>
2023-04-18 22:44:53 +0800

    Update batc.py

--- batc.py
+++ batc.py
@@ -6,6 +6,6 @@ import os
 
 file_ = ["train1", "train2", "train3", "train4", "train5"]
 # file_ = ["0331","0332","0333","0334"]
-os.system("python train_mnist.py -data mnist -G_type convT -num_class 10 -nb_cl_fg 2 -num_task 4 "
-              "-BatchSize 100  -tradeoff 1 -epochs 101 -lr 1e-3  -epochs_gan 301  -lr_decay_step 20 -gan_lr 0.0002 "
-              "-gan_lr_decay_step 150 -log_dir mnist10_5_linear -dir D:/Dataset -gpu 0")
+os.system("python train_mnist.py -data fashion_mnist -G_type convT -num_class 10 -nb_cl_fg 2 -num_task 4 "
+              "-BatchSize 100  -tradeoff 1 -epochs 101 -lr 1e-3  -epochs_gan 301  -lr_decay_step 100 -gan_lr 0.0002 "
+              "-gan_lr_decay_step 150 -log_dir mnist10_5_con -dir /kaggle/input/fashionmnist -gpu 0")
